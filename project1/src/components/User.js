8be11d38fdfa404248094058fa3736d1820578b9
mateu52 <wuoelte@gmail.com>
2021-11-14 01:37:39 +0100

    zad1

--- project1/src/components/User.js
+++ project1/src/components/User.js
@@ -3,7 +3,9 @@ import React from 'react';
 function User(){
 
     return(
-
+        <div>
+            <p>witam</p>
+        </div>
     )
 }
 
--- project1/src/components/UserList.js
+++ project1/src/components/UserList.js
@@ -3,7 +3,9 @@ import React from 'react';
 function UserList(){
 
     return(
-
+        <div>
+            <p>witam</p>
+        </div>
     )
 }
 
