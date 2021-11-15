9018779b5952c96789d97c0f4dce35fe0b5e932f
mateu52 <wuoelte@gmail.com>
2021-11-15 12:05:19 +0100

    zad1

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
 import App1 from './components/App1';
-import UserList from './components/UserList';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi</h1>
-      <UserList />
+      <App1 />
     </div>
   );
 }
--- project1/src/components/UserList.js
+++ project1/src/components/UserList.js
@@ -1,10 +1,23 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 
 function UserList(){
     const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('https://randomuser.me/api')
+        /* .then(response => { 
+            return response.json();
+        }) */
+        .then ((data) => 
+            setUsers(data.results)) //{}?
+        .catch(error => console.error(error));
+    },[]);
     return(
         <div>
-            <p>witam</p>
+            {users.map(users => (
+                <div>
+                    {users}
+                </div>
+            ))}
         </div>
     )
 }
