bc4150af0365bd23d8ad67be7c8eee26b083412d
mateu52 <wuoelte@gmail.com>
2021-11-17 13:04:46 +0100

    zad1

--- project1/src/components/UserList.js
+++ project1/src/components/UserList.js
@@ -3,21 +3,19 @@ import React,{useState, useEffect} from 'react';
 function UserList(){
     const [users, setUsers] = useState([]);
     useEffect(() => {
-        fetch('https://randomuser.me/api')
-        /* .then(response => { 
+        fetch('https://randomuser.me/api/?results=1')
+        .then(response => { 
             return response.json();
-        }) */
+        })
         .then ((data) => 
-            setUsers(data.results)) //{}?
+            setUsers(data)) //{}?
         .catch(error => console.error(error));
     },[]);
     return(
         <div>
-            {users.map(users => (
-                <div>
-                    {users}
-                </div>
-            ))}
+           
+                return <h1>{users}</h1>
+           
         </div>
     )
 }
